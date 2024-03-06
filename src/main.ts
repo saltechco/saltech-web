1cf26c472bec9bd4243e5fcdfca9438911b2cb52
Saleh Masoudi <m.smasoudi72@gmail.com>
2024-03-06 08:15:31 +0330

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import './assets/main.css'
+import 'primevue/resources/themes/aura-light-green/theme.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import PrimeVue from 'primevue/config'
+
+
+const app = createApp(App)
+
+app.use(router)
+app.use(PrimeVue)
+
+app.mount('#app')
